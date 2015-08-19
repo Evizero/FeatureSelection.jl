a0824be45daf8b10d6daeb1d048795d6e5d5951f
Christof Stocker <christof.stocker@aimc.at>
2015-08-19 17:10:39 +0200

    add const annotations for compiler

--- src/measures.jl
+++ src/measures.jl
@@ -1,10 +1,9 @@
-log2π = log(2π)
+const log2π = log(2π)
 
 function aic{F<:Real,R<:Real}(y::Vector{F}, y_hat::Vector{R}, p::Int)
-  n = length(y)
+  const n = length(y)
   n == length(y_hat) || throw(DimensionMismatch("y and y_hat have to be of the same size"))
-  r = y_hat - y
-  tsum = dot(r,r)
-  n * (log2π + 1 + log(tsum/n)) + 2(p+1)
+  const r = y_hat - y
+  n * (log2π + 1 + log(dot(r,r)/n)) + 2(p+1)
 end
 
