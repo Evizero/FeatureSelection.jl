944e7bbeb99bb8d9118c8cd4eaa6ed99ff92ff58
Christof Stocker <christof.stocker@aimc.at>
2015-08-19 19:17:08 +0200

    add require and refactor AIC and BIC

--- src/measures.jl
+++ src/measures.jl
@@ -1,17 +1,23 @@
 const log2π = log(2π)
 
-function aic{F<:Real,R<:Real}(y::Vector{F}, y_hat::Vector{R}, p::Int, k::FloatingPoint=2.)
+"""
+The Akaike information criterion
+"""
+function aic{F<:Real,R<:Real}(y::Vector{F}, ŷ::Vector{R}, p::Int, k::FloatingPoint=2.)
   const n = length(y)
-  n == length(y_hat) || throw(DimensionMismatch("y and y_hat have to be of the same size"))
-  const r = y_hat - y
+  n == length(ŷ) || throw(DimensionMismatch("y and y_hat have to be of the same size"))
+  const r = ŷ - y
   n * (log2π + 1 + log(dot(r,r)/n)) + k*(p+1)
 end
 
-function bic{F<:Real,R<:Real}(y::Vector{F}, y_hat::Vector{R}, p::Int)
+"""
+The Bayesian information criterion, also known as Schwarz's Bayesian criterion.
+"""
+function bic{F<:Real,R<:Real}(y::Vector{F}, ŷ::Vector{R}, p::Int)
   const n = length(y)
-  n == length(y_hat) || throw(DimensionMismatch("y and y_hat have to be of the same size"))
+  n == length(ŷ) || throw(DimensionMismatch("y and y_hat have to be of the same size"))
   const k = log(n)
-  const r = y_hat - y
-  n * (log2π + 1 + log(dot(r,r)/n)) + k*(p+1)
+  const r = ŷ - y
+  n * (log2π + 1 + log(dot(r,r)) - k) + k*(p+1)
 end
 
